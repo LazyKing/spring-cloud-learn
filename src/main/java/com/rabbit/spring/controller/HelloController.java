04ad25cfaf8961a7caa7281e668757dc61b1b8f6
guoyankui <yankuiguo@gmail.com>
2016-09-08 23:30:52 +0800

    add spring boot aop usage

--- src/main/java/com/rabbit/spring/controller/HelloController.java
+++ src/main/java/com/rabbit/spring/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.rabbit.spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,13 @@ import com.rabbit.spring.model.Person;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-
+	private Logger log = LoggerFactory.getLogger(HelloController.class);
 	@Autowired
 	private PersonRepository personRepository;
 	
 	@RequestMapping("/person")
 	public Person person(String name){
+		log.info("请求的参数name" + name);
 		return personRepository.findByName(name);
 	}
 	
