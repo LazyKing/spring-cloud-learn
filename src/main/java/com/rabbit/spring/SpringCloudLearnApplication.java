2507bad302142b8a80428a1f0704b1c6f2d16c26
guoyankuiBASH <yankuiguo@gmail.com>
2016-09-08 10:54:10 +0800

    init a project to learn using thymeleaf in spring cloud (boot)

---
+++ src/main/java/com/rabbit/spring/SpringCloudLearnApplication.java
@@ -0,0 +1,12 @@
+package com.rabbit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLearnApplication.class, args);
+	}
+}
---
+++ src/main/java/com/rabbit/spring/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.rabbit.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rabbit.spring.dao.PersonRepository;
+import com.rabbit.spring.model.Person;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@Autowired
+	private PersonRepository personRepository;
+	
+	@RequestMapping("/person")
+	public Person person(String name){
+		return personRepository.findByName(name);
+	}
+}
