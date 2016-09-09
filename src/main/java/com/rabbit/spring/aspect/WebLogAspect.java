592cf605a61a2c7750da92247b4745e4cc86d29b
guoyankuiBASH <yankuiguo@gmail.com>
2016-09-09 11:28:18 +0800

    add threadlocal variable in aop aspect

--- src/main/java/com/rabbit/spring/aspect/WebLogAspect.java
+++ src/main/java/com/rabbit/spring/aspect/WebLogAspect.java
@@ -21,27 +21,30 @@ public class WebLogAspect {
 
 	private Logger log = LoggerFactory.getLogger(WebLogAspect.class);
 	
+	ThreadLocal<Long> startTime = new ThreadLocal<Long>();
+	
 	@Pointcut("execution(public * com.rabbit.spring.controller..*.*(..))")
 	public void webLog(){}
 	
 	@Before("webLog()")
 	public void doBefore(JoinPoint joinPoint) throws Exception{
+		startTime.set(System.currentTimeMillis());
 		// 接收到请求，记录请求内容
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
 
         // 记录下请求内容
-        log.info("URL : " + request.getRequestURL().toString());
-        log.info("HTTP_METHOD : " + request.getMethod());
-        log.info("IP : " + request.getRemoteAddr());
-        log.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        log.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+        log.info("request info >> URL:{}, HTTP_METHOD:{}, REMOTE_IP:{}, CLASS_METHOD:{}, ARGS:{}", 
+        		request.getRequestURL().toString(), request.getMethod(), request.getRemoteAddr(), 
+        		joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName(), 
+        		Arrays.toString(joinPoint.getArgs()));
 	}
 	
 	@AfterReturning(returning = "ret", pointcut = "webLog()")
     public void doAfterReturning(Object ret) throws Throwable {
         // 处理完请求，返回内容
         log.info("RESPONSE : " + com.alibaba.fastjson.JSONObject.toJSONString(ret));
+        log.info("request timed (ms):{}", System.currentTimeMillis() - startTime.get());
     }
 	
 }
