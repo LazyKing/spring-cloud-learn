ac8d700813a98484c96c16b65c7a63f2f46367b4
guoyankuiBASH <yankuiguo@gmail.com>
2016-09-12 18:26:58 +0800

    add eureka client usage

--- src/main/java/com/rabbit/spring/SpringCloudLearnApplication.java
+++ src/main/java/com/rabbit/spring/SpringCloudLearnApplication.java
@@ -2,7 +2,9 @@ package com.rabbit.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringCloudLearnApplication {
 
